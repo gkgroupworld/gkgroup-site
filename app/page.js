1cf34ab7dde910b9dc286261f64366516f502617
gkgroupworld <gk.contactus@gkgroup.app>
2026-05-11 11:58:53 +0000

    Add WhatsApp button

--- app/page.js
+++ app/page.js
@@ -183,7 +183,12 @@ export default function Home() {
 
         </div>
       </section>
-
+<a
+  href="https://wa.me/22890559888"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white px-5 py-4 rounded-full shadow-2xl text-lg font-bold z-50"
+>
+  WhatsApp
+</a>
     </main>
-  );
-}
